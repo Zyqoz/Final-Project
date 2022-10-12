f7e4c24566d1f9f68a5c06e190719810b8e9d330
Mir Muhammad Abdur Rahman <98369241+Zyqoz@users.noreply.github.com>
2022-10-12 15:52:49 -0400

    1 edit

---
+++ montecarlo.py
@@ -0,0 +1,7 @@
+class Die():
+    """ 
+    This class instantiates a die with N sides and W weights, and the die can be rolled to select a face
+    """
+    
+    def __init__(self, faces):
+        
