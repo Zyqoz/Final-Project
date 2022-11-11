2048a29873c74b233ac0a69e0b681db2e3dd4b9a
Mir Muhammad Abdur Rahman <98369241+Zyqoz@users.noreply.github.com>
2022-11-10 19:32:13 -0500

    Final Project Changes

--- montecarlo.py
+++ montecarlo.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 class Die():
     """ 
     This class instantiates a die with N sides and W weights, and the die can be rolled to select a face
@@ -5,3 +7,14 @@ class Die():
     
     def __init__(self, faces):
         
+        if not faces.dtype == 'i' or not faces.dtype == 'S':
+            return 'Incorrect dtype'
+        
+        if not len(np.unique(faces)) == len(faces):
+            return 'The elements of the faces must be unique'
+        
+        weights = fill(len(faces), 1, dtype = float)
+        
+        df = pd.DataFrame(faces,weights)
+        
+        return df
